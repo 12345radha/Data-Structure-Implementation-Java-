2a7d6ff6cbad4f49d8a99a87f7479e69cdb6581b
Radha Kumari <56878565+12345radha@users.noreply.github.com>
2024-03-02 09:48:50 +0530

    Create Node.java

---
+++ Stack/Node.java
@@ -0,0 +1,17 @@
+package com.jsp.Stack;
+
+
+public class Node {
+	Object ele;
+	Node next;
+	
+	Node(Object ele,Node next)
+	{
+		this.ele=ele;
+		this.next=next;
+	}
+	
+	Node(Object e){
+		this.ele=e;
+	}
+}
